6c1f6e5e220849173ef54278d0facafc29f172a0
slodge <lodge.stuart@gmail.com>
2011-10-13 13:50:34 +0100

    click changes - more to come!

--- Client/AutomationClient/Remote/ClickCommand.cs
+++ Client/AutomationClient/Remote/ClickCommand.cs
@@ -10,6 +10,9 @@
 // ------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Automation.Peers;
 using System.Windows.Automation.Provider;
 
@@ -17,6 +20,20 @@ namespace WindowsPhoneTestFramework.AutomationClient.Remote
 {
     public partial class ClickCommand
     {
+        private static List<Func<AutomationPeer, bool>> PatternTesters;
+
+        static ClickCommand()
+        {
+            var providers =
+                from method in typeof (ClickCommand).GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
+                where method.ReturnType == typeof (bool)
+                      && method.GetParameters().Length == 1
+                      && method.GetParameters()[0].ParameterType == typeof (AutomationPeer)
+                select new Func<AutomationPeer,bool>((AutomationPeer peer) => (bool)method.Invoke(null, new object[] {peer}));
+
+            PatternTesters = new List<Func<AutomationPeer, bool>>(providers);
+        }
+        
         protected override void DoImpl()
         {
             var element = AutomationElementFinder.FindElement(AutomationIdentifier);
@@ -26,14 +43,28 @@ namespace WindowsPhoneTestFramework.AutomationClient.Remote
                 return;
             }
 
-            // automate the click?
+            // automate the click
             var peer = AutomationPeerCreator.GetPeer(element);
             if (peer == null)
                 throw new TestAutomationException("No automation peer found for " + element.GetType().FullName);
 
+            foreach (var patternTester in PatternTesters)
+            {
+                if (patternTester(peer))
+                {
+                    SendSuccessResult();
+                    return;
+                }
+            }
+
+            throw new TestAutomationException("No invoke pattern found for " + element.GetType().FullName);
+        }
+
+        private static bool TryInvokePatternAutomation(AutomationPeer peer)
+        {
             var pattern = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
             if (pattern == null)
-                throw new TestAutomationException("No invoke pattern found for " + element.GetType().FullName);
+                return false;
 
             try
             {
@@ -44,7 +75,25 @@ namespace WindowsPhoneTestFramework.AutomationClient.Remote
                 throw new TestAutomationException("Exception while invoking pattern", exception);
             }
 
-            SendSuccessResult();
+            return true;
+        }
+
+        private static bool TryTogglePatternAutomation(AutomationPeer peer)
+        {
+            var pattern = peer.GetPattern(PatternInterface.Toggle) as IToggleProvider;
+            if (pattern == null)
+                return false;
+
+            try
+            {
+                pattern.Toggle();
+            }
+            catch (Exception exception)
+            {
+                throw new TestAutomationException("Exception while invoking pattern", exception);
+            }
+
+            return true;
         }
     }
 }
