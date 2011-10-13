2619aa0d12bf2184a99fe6b0b876e16dac454e9d
slodge <lodge.stuart@gmail.com>
2011-10-13 09:57:47 +0100

    Just formatting

--- CommandLine/CommandLineHost/Commands/ConsoleCommands.cs
+++ CommandLine/CommandLineHost/Commands/ConsoleCommands.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------
+// <copyright file="ConsoleCommands.cs" company="Expensify">
+//     (c) Copyright Expensify. http://www.expensify.com
+//     This source is subject to the Microsoft Public License (Ms-PL)
+//     Please see license.txt on https://github.com/Expensify/WindowsPhoneTestFramework
+//     All other rights reserved.
+// </copyright>
+// 
+// Author - Stuart Lodge, Cirrious. http://www.cirrious.com
+// ------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 
--- CommandLine/EmuHost/Commands/DriverCommands.cs
+++ CommandLine/EmuHost/Commands/DriverCommands.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------
+// <copyright file="DriverCommands.cs" company="Expensify">
+//     (c) Copyright Expensify. http://www.expensify.com
+//     This source is subject to the Microsoft Public License (Ms-PL)
+//     Please see license.txt on https://github.com/Expensify/WindowsPhoneTestFramework
+//     All other rights reserved.
+// </copyright>
+// 
+// Author - Stuart Lodge, Cirrious. http://www.cirrious.com
+// ------------------------------------------------------------------------
+
+using System;
 using System.ComponentModel;
 using WindowsPhoneTestFramework.EmuDriver;
 
