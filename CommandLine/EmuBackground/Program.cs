0d962e5968d104ccd2f26022f10d59a71dfa3157
slodge <lodge.stuart@gmail.com>
2011-10-15 18:54:51 +0100

    Simple tools to help with setting emu as foreground window

---
+++ CommandLine/EmuBackground/Program.cs
@@ -0,0 +1,24 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Expensify">
+//     (c) Copyright Expensify. http://www.expensify.com
+//     This source is subject to the Microsoft Public License (Ms-PL)
+//     Please see license.txt on https://github.com/Expensify/WindowsPhoneTestFramework
+//     All other rights reserved.
+// </copyright>
+// 
+// Author - Stuart Lodge, Cirrious. http://www.cirrious.com
+// ------------------------------------------------------------------------
+
+using WindowsPhoneTestFramework.EmuDriver;
+
+namespace WindowsPhoneTestFramework.EmuBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var displayInputController = new EmulatorDisplayInputController();
+            displayInputController.ReleaseWindowFromForeground();
+        }
+    }
+}
